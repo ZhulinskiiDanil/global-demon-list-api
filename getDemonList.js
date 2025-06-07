0b13cac6766d7676b4f60ea945109e70127f5f16
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2025-06-07 12:28:01 +0200

    chore: update

--- getDemonList.js
+++ getDemonList.js
@@ -1,19 +1,10 @@
-import fs from 'fs';
 import puppeteer from 'puppeteer';
-
-const chromePath = fs
-  .readdirSync('/opt/render/.cache/puppeteer/chrome/')
-  .map(
-    (dir) => `/opt/render/.cache/puppeteer/chrome/${dir}/chrome-linux64/chrome`
-  )
-  .find(fs.existsSync);
 
 export default async function getDemonList() {
   const url = 'https://demonlist.org/';
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: chromePath,
   });
   const page = await browser.newPage();
 
